86bab1494863414cd593e5c13a02bbe3f9e5a985
Lavneet Hora <31lavneet@gmail.com>
2025-04-15 19:03:38 -0500

    Create Lab3_Q94.c

---
+++ Lab3_Q94.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void inorder(struct TreeNode* root, int* result, int* index) {
+    if (root == NULL) return;
+    inorder(root->left, result, index);
+    result[(*index)++] = root->val;
+    inorder(root->right, result, index);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(100 * sizeof(int)); // max nodes = 100
+    int index = 0;
+    
+    inorder(root, result, &index);
+    
+    *returnSize = index;
+    return result;
+}
